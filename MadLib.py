30ffc7878a14a48e4792d35177aed3d682e4a577
Malakai107354 <Malakai107354@mcpsva.org>
2026-08-28 15:17:59 -0400

    Send

---
+++ MadLib.py
@@ -0,0 +1,8 @@
+#input("I can hardly wait to go to school today because I have something special to show my friends! It's my new (noun) that I've been (verb ending in-ing) for (number) (time unit). I know they're going to be so (emotion) when they see it.")
+
+verb1=input("enter a VERB: ")
+name1=input("enter a NAME: ")
+color1=input("enter a color:")
+print("I",verb1, "today.")
+print("I saw",name1,"today too!")
+print("I heard", color1, " is his favorite color. ")
