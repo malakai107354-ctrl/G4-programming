9124e8261c48c0c2223507496c905a687fdadba6
Malakai107354 <Malakai107354@mcpsva.org>
2026-09-14 15:12:53 -0400

    send

---
+++ Simple calculator.py
@@ -0,0 +1,6 @@
+x1=int(input("Enter first number"))
+x2=int(input("Enter second number"))
+print(f"{x1} + {x2} = ",(x1+x2))
+print(f"{x1} - {x2} = ",(x1-x2))
+print(f"{x1} * {x2} = ",(x1*x2))
+print(f"{x1} / {x2} = ",(x1/x2))
