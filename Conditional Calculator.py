3bcec2d8877a40fd48fbd19c25ed1311b4d376aa
Malakai107354 <Malakai107354@mcpsva.org>
2026-09-18 15:08:32 -0400

    send

---
+++ Conditional Calculator.py
@@ -0,0 +1,19 @@
+print("Welcome to the Calculator")
+
+n1=int(input("Enter your first number"))
+op=input("Enter an operation(+,-,*,/)")
+n2=int(input("Enter your second number"))
+
+if op == "+":
+    print(n1+n2)
+    #Code goes here
+elif op == "-":
+    print(n1-n2)
+    #Code goes here
+elif op == "*":
+    print(n1*n2)
+    #Code goes here
+elif op == "/":
+    print(n1/n2)
+    #Code goes here
+
